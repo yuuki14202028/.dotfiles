9bb756f6e1447215471da1b21679bee3a5471a09
yuuki14202028 <yuuki14202028@gmail.com>
2024-06-27 19:44:02 +0900

    Fixed Allow UnFree

--- configuration.nix
+++ configuration.nix
@@ -10,11 +10,13 @@
       ./hardware-configuration.nix
     ];
 
-  allowUnfree = true;
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
