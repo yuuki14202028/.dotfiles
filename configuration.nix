0d443dc389d1dbbbc29246e512736025aeee2e16
yuuki14202028 <yuuki14202028@gmail.com>
2024-06-27 19:23:41 +0900

    Third Fixed Commit.

--- configuration.nix
+++ configuration.nix
@@ -146,7 +146,7 @@
     fontconfig = {
       defaultFonts = {
         serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
-	sanSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
+	sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
 	monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
 	emoji = ["Noto Color Emoji"];
       };
