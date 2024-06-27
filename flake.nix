bb56787e31b182b1454b62e4098c5c91361abc93
yuuki14202028 <yuuki14202028@gmail.com>
2024-06-27 19:57:06 +0900

    Install Home Manager

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: {
@@ -10,6 +14,20 @@
         modules = [
           ./configuration.nix
         ];
+      };
+    };
+    homeConfigurations = {
+      myHome = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+	  system = "x86_64-linux";
+	  config.allowUnfree = true;
+	};
+	extraSpecialArgs = {
+	  inherit inputs;
+	};
+	modules = [
+	  ./home.nix
+	];
       };
     };
   };
