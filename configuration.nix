bf50b53fddf81b006624391e3e6261bd224f4268
yuuki14202028 <yuuki14202028@gmail.com>
2024-11-21 21:08:11 +0900

    Install Tailscale

--- configuration.nix
+++ configuration.nix
@@ -50,6 +50,15 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  services.tailscale.enable = true;
+  networking.firewall = {
+   enable = true;
+   # tailscaleの仮想NICを信頼する
+   # `<Tailscaleのホスト名>:<ポート番号>`のアクセスが可能になる
+   trustedInterfaces = ["tailscale0"];
+   allowedUDPPorts = [config.services.tailscale.port];
+  };
+
   # Set your time zone.
   time.timeZone = "Asia/Tokyo";
 
